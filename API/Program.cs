9ce0ec2b0ab2d28914d6fc257c1029e000fc5c3b
Ashwathselvam <ashwathselvam@gmail.com>
2023-08-17 12:36:13 +0530

    eos4

--- API/Program.cs
+++ API/Program.cs
@@ -1,27 +1,11 @@
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
-var CorsPolicy = "CorsPolicy";
+
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt=>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: CorsPolicy,
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
-        });
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -32,9 +16,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
-app.UseCors(CorsPolicy);
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
- 
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
